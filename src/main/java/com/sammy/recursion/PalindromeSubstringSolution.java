9ba660dfff32a6875f23c37f3faf35a56b0626d0
Samuel Ifere <sml.ifere@gmail.com>
2022-09-02 09:08:49 +0100

    Adds logic to recursively check for Palindrome words

---
+++ src/main/java/com/sammy/recursion/PalindromeSubstringSolution.java
@@ -0,0 +1,24 @@
+package com.sammy.recursion;
+
+public class PalindromeSubstringSolution {
+    private static boolean isPalindrome(String word){
+        boolean isWordPalindrome;
+        if(word.length() <= 1){
+            isWordPalindrome = true;
+        }else if(word.charAt(0) != word.charAt(word.length() - 1)){
+            isWordPalindrome = false;
+        }else{
+            isWordPalindrome = isPalindrome(word.substring(1, word.length() - 1));
+        }
+
+        return isWordPalindrome;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Is moon a palindrome? " + isPalindrome("moon"));
+        System.out.println("Is noon a palindrome? " + isPalindrome("noon"));
+        System.out.println("Is a a palindrome? " + isPalindrome("a"));
+        System.out.println("Is aba a palindrome? " + isPalindrome("aba"));
+        System.out.println("Is ab a palindrome? " + isPalindrome("ab"));
+    }
+}
